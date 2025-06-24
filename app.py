7f5a7c25ccfd1c15daa594ee1516699a5d56ca93
Suhaila419 <163913554+Suhaila419@users.noreply.github.com>
2025-06-24 12:05:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ if st.button("Check if Spam"):
 
 # Run Streamlit via ngrok
 from pyngrok import ngrok
-!streamlit run app.py &
 
 public_url = ngrok.connect(port=8501)
 print(f"🔗 Your app is live at: {public_url}")
